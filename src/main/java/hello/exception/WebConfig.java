bd0ba13cf793f0690f472870678c784d29e30bd0
minkyung kim <minry20415@naver.com>
2024-12-25 15:02:27 +0900

    ✨: API 예외처리
    - 지금까지 했던 예외처리는 클라이언트에게 에러 페이지만 보여주면 끝이라 html 코드를 던져주면됐는데 API 호출 시 예외처리는 JSON 형식으로 어떤 에러가 있는지 내용을 정형화하여 전달해야함

--- src/main/java/hello/exception/WebConfig.java
+++ src/main/java/hello/exception/WebConfig.java
@@ -5,13 +5,12 @@ import hello.exception.interceptor.LogInterceptor;
 import jakarta.servlet.DispatcherType;
 import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WbConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
