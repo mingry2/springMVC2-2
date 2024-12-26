afa4e7c9109778cb45a1ad3de95917777d58286e
minkyung kim <minry20415@naver.com>
2024-12-26 10:38:39 +0900

    ✨: 스프링 타입 컨버터를 위한 리팩터링 & 컨트롤러 생성

--- src/main/java/hello/exception/servlet/ErrorPageController.java
+++ src/main/java/hello/exception/servlet/ErrorPageController.java
@@ -6,12 +6,10 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.awt.*;
 import java.util.HashMap;
 import java.util.Map;
 
