3070b03ba402e6c1ad0b9c1c14da91dcb64e9827
minkyung kim <minry20415@naver.com>
2024-12-26 16:30:45 +0900

    ✨: 포멧터(Formatter)
    - 객체를 특정한 포멧에 맞추어 출력하거나 또는 그 반대의 역할을 하는 것에 특화된 기능
    - Converter 범용 (객체->객체)
    - Formatter 문자에 특화 (객체->문자, 문자->객체)+현지화(Locale)
    - Formatter의 parse(), print() 구현
    - 컨버전 서비스는 포맷터를 지원함
    ㄴ addConverter(), convert()를 사용하여 컨버터와 동일하게 사용가능
    - 스프링이 제공하는 기본 포맷터
    1. @NumberFormat : 숫자 관련 형식 지정
    - NumberFormatAnnotationFormatterFactory
    2. @DateTimeFormat : 날짜 관련 형식 지정
    - Jsr310DateTimeFormatAnnotationFormatterFactory

--- src/main/java/hello/exception/WebConfig.java
+++ src/main/java/hello/exception/WebConfig.java
@@ -8,6 +8,7 @@ import hello.exception.typeconverter.converter.IntegerToStringConverter;
 import hello.exception.typeconverter.converter.IpPortToStringConverter;
 import hello.exception.typeconverter.converter.StringToIntegerConverter;
 import hello.exception.typeconverter.converter.StringToIpPortConverter;
+import hello.exception.typeconverter.formatter.MyNumberFormatter;
 import jakarta.servlet.DispatcherType;
 import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
@@ -25,10 +26,12 @@ public class WebConfig implements WebMvcConfigurer {
     // 컨버터 등록 -> 스프링 내부에서 사용하는 컨버전에 추가
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntegerToStringConverter());
+        // 주석처리 -> 우선순위
+//        registry.addConverter(new StringToIntegerConverter());
+//        registry.addConverter(new IntegerToStringConverter());
         registry.addConverter(new IpPortToStringConverter());
         registry.addConverter(new StringToIpPortConverter());
+        registry.addFormatter(new MyNumberFormatter());
     }
 
     /* 스프링 인터셉터, HandlerExceptionResolver
